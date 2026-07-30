60a7bbf8eb465ee94b28e43e41c242ef29d5ee91
Ming <2847919699@qq.com>
2026-07-30 22:17:39 +0800

    feat:初始化项目

---
+++ src/main/java/org/mingharness/MingHarnessApplication.java
@@ -0,0 +1,13 @@
+package org.mingharness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingHarnessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MingHarnessApplication.class, args);
+    }
+
+}
